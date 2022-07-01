44cf41c32ded7eb5f11a6eef6cd1caa0631cf7e9
bukesisay <bsisay37@gmail.com>
2022-07-01 15:06:43 -0700

    base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ *main - is funtion
+ *Return: ch
+ */
+
+int main(void)
+{
+int n = 0;
+char a = 'a';
+ while (n < 10)
+{
+putchar(n%10);
+n++;
+}
+ while ( a <= 'f')
+   {
+     putchar(a);
+     a++;
+   }
+putchar('\n');
+return (0);
+}
---
+++ 0x01-variables_if_else_while/8-print_base16.c~
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ *main - is funtion
+ *Return: ch
+ */
+
+int main(void)
+{
+int n = 0;
+while (n < 10)
+{
+putchar(n%10);
+n++;
+}
+putchar('\n');
+return (0);
+}
