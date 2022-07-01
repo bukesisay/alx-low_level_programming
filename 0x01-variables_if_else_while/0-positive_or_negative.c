804be9fa95db383abcdfb8f319b47acd0d753158
bukesisay <bsisay37@gmail.com>
2022-07-01 13:05:21 -0700

    fix printouts

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -14,16 +14,8 @@ int n;
 
 srand(time(0));
 n = rand() - RAND_MAX / 2;
-if (n > 0) {
-  printf("%d is positie number", n);
-}
-if (n < 0) {
-  printf("%d is negative number", n);
-}
-else
-{
-  printf("%d is zero", n);
-}
-
+if (n > 0) printf("%d is positive");
+ if (n < 0) printf("%d is negative number")
+else printf("%d is zero")
 return (n);
 }
