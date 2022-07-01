44be07bed4a42d6c2cc9fbeb5d8d416b905582a1
bukesisay <bsisay37@gmail.com>
2022-07-01 13:00:12 -0700

    add

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -14,14 +14,14 @@ int n;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 if (n > 0) {
-  print("%d is positie number", n);
+  printf("%d is positie number", n);
 }
 if (n < 0) {
-  print ("%d is negative number", n);
+  printf("%d is negative number", n);
 }
 else
 {
-  print ("%d is zero", n);
+  printf("%d is zero", n);
 }
 
 return (n);
