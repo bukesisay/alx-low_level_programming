37228636faec763f4cb66328790831212e1a3b9d
bukesisay <bsisay37@gmail.com>
2022-07-01 12:56:20 -0700

    print  number status

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -13,17 +13,15 @@ int n;
 
 srand(time(0));
 n = rand() - RAND_MAX / 2;
-if (n > 0)
-{
-print "%d is positie number", n;
+if (n > 0) {
+  print("%d is positie number", n);
 }
-if (n < 0)
-{
-print "%d is negative number", n;
+if (n < 0) {
+  print ("%d is negative number", n);
 }
 else
 {
-print "%d is zero", n;
+  print ("%d is zero", n);
 }
 
 return (n);
