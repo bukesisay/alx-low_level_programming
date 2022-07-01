45a953ff03995a3788e48399d8f83c2218b0031c
bukesisay <bsisay37@gmail.com>
2022-07-01 09:08:49 -0700

    qewithout

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main()
+{
+
+  char ch = 'a';
+  if (ch != q && e)
+  {
+    while (ch <= 'z')
+      {
+
+	putchar(ch);
+	ch++;
+      }
+   }
+  putchar(\n)
+    return 0;
+
+}
