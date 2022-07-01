c44f0e13b78647684c43980df11142fc171682ad
bukesisay <bsisay37@gmail.com>
2022-07-01 14:59:54 -0700

    print alphabets

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+/**
+*main - is function
+*Return: ch
+*/
+int main(void)
+
+{
+char ch = 'z';
+{
+while (ch <= 'a')
+{
+putchar(ch);
+ch--;
+}
+putchar('\n');
+}
+return (0);
+}
