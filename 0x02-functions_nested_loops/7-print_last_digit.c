dae608401ccd0405710d9d4b6c59e5ed01d5fa9f
bukesisay <bsisay37@gmail.com>
2022-07-05 11:16:38 -0700

    print last

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,11 +1,11 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
- * print_last_digit - Check Holberton
- * @r: An integer input
- * Description: This function prints the last digit of a 
- * Return: last digit of number r
- */
+* print_last_digit - Check main
+* @r: An integer input
+* Description: This function prints
+* Return: last digit of number 
+*/
 int print_last_digit(int r)
 {
 int n;
