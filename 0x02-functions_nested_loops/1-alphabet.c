d77416394023e830a4c1a0594d00874b3b68ab86
bukesisay <bsisay37@gmail.com>
2022-07-05 10:26:38 -0700

    alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,8 +1,8 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
  * print_alphabet - Check description
- * Description: It prints the alphabet in lowercase fallowed by a new line
+ * Description: It prints the alphabet in lowercase fallowed
  * Return: Nothing.
  */
 void print_alphabet(void)
