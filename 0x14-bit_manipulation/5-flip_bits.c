b9dedf955f34f121307c6e3cbe70b69b0451ba8e
bukesisay <bsisay37@gmail.com>
2022-08-18 10:38:14 -0700

    flip

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -6,7 +6,7 @@
  * @m: The number to set other equal
  * Return: The number of fliped bits
  */
-unsigned int flip_bits(unsigned long int n, unsigned long int m);
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
 unsigned int flips = 0;
 unsigned long int xor = (n ^ m);
