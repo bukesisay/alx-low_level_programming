606e43c4ad98474c2fe1aeb9baa4ad6691ffe4f4
biruke sisay <bsisay37@gmail.com>
2022-10-25 10:31:46 -0700

    hash

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,6 +1,5 @@
 #include "hash_tables.h"
 #include <stdlib.h>
-
 /**
  * hash_table_create - A function that creates a hash table.
  * @size: The size of hash table.
@@ -10,7 +9,6 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 unsigned long int i = 0;
 hash_table_t *new_table = NULL;
-
 new_table = malloc(sizeof(hash_table_t));
 if (!new_table)
 return (NULL);
